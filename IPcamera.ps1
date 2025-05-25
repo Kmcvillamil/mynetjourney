d71db19118b3ca2e940f3ff9c8cce52313b4c1f1
Kmcvillamil <kmcvillamil@outlook.com>
2025-05-25 16:04:53 +0800

    Add files via upload

---
+++ IPcamera.ps1
@@ -0,0 +1,10 @@
+﻿config t
+ip routing
+ip dhcp pool CAMERA6
+host 10.62.50.6 255.255.255.0
+client-identifier 001a.0706.c302   
+ip dhcp pool CAMERA8
+host 10.62.50.8 255.255.255.0
+client-identifier 001a.070b.472e
+end
+show ip dhcp binding
---
+++ IPphoneconfirg.ps1
@@ -0,0 +1,40 @@
+﻿config t   
+no telephony-service
+telephony-service
+   no auto assign
+   no auto-reg-ephone
+   max-ephones 5
+   max-dn 20
+   ip source-address 10.62.100.8 port 2000
+   create cnf-files
+ephone-dn 1
+  number 6211
+ephone-dn 2
+  number 6222
+ephone-dn 3
+  number 6233
+ephone-dn 4
+  number 6244
+ephone-dn 5
+  number 6255
+ephone-dn 6
+  number 6266
+ephone-dn 7
+  number 6277
+ephone-dn 8
+  number 6288
+ ephone-dn 9
+   number 6299
+ephone-dn 10
+ number 6298
+Ephone 1
+  Mac-address 203a.0782.bd3b
+  type 8945
+  button 1:8 2:7 3:6 4:5
+  restart
+Ephone 2
+  Mac-address ccd8.c1fb.1519
+  type 8945
+  button 1:4 2:3 3:2 4:1
+  restart  
+end
